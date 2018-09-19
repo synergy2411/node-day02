ed6c0a8a70472e0e4fc3ae92376a81f76de57954
synergy2411 <synergy2411@outlook.com>
2018-09-19 13:07:13 +0530

    jade template added

---
+++ template-server.js
@@ -0,0 +1,15 @@
+var app = require("express")();
+var jade = require("jade");
+
+app.set("view engine", jade);
+app.set("views", __dirname+'/views');
+
+var friends = ["foo","bar","bam","bas"]
+
+app.get("/", function(req, res){
+    res.render("index.jade", {"name": "Bob", "friends" : friends});
+})
+
+app.listen(9000, function(){
+    console.log("Server running on port 9000...");
+})
